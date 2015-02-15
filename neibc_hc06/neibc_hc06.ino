7c69062a957111eeebd227217d826eeec4290f4c
bbangpan <tpfs357@gmail.com>
2015-02-16 01:30:53 +0900

    Arduino sample codes
    www.bbangpan.com

---
+++ neibc_hc06/neibc_hc06.ino
@@ -0,0 +1,36 @@
+/* Originally posted on www.bbangpan.com
+   Program Description: HC-06 bluetooth 2.0 test code
+   DataPin : 2,3 to TX, RX of HC-06(cross connection)
+   Tested on : Arduino 1.0.6, Arduino UNO R3, HC-06
+   
+   Copyright (c) 2015 www.bbangpan.com. All rights reserved.
+   This program can be used for any non-commercial purpose freely.   */
+   
+#include <SoftwareSerial.h>
+  
+SoftwareSerial BTSerial(2, 3); // SoftwareSerial(RX, TX)
+  
+void setup()  
+{
+  Serial.begin(9600);
+  Serial.println("Hello!");
+
+  // set the data rate for the BT port
+  BTSerial.begin(9600);
+}
+void loop(){
+  while (BTSerial.available()){ // if BT sends something
+    byte data = BTSerial.read();
+    Serial.write(data); // write it to serial(serial monitor)
+  }
+  
+  while (Serial.available()){ // if Serial has input(from serial monitor)
+    byte data = Serial.read();
+    if(data == '\n') {
+      data=0;
+    }
+    BTSerial.write(data); // write it to BT
+  }
+  
+}
+
