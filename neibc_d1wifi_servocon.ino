53e725f7270ae7f3d2df0179251e5d2362362d7b
bbangpan <tpfs357@gmail.com>
2019-08-04 01:58:25 +0900

    arduino sample codes

---
+++ neibc_d1wifi_servocon.ino
@@ -0,0 +1,68 @@
+/* Originally posted on www.bbangpan.com
+   Program Description: Mobile browser controlled servo motor on WeMos D1 WiFi board
+   Tested on : Arduino 1.8.5, Arduino Compatible D1 WiFi, Servo Motor
+   
+   Copyright (c) 2019 www.bbangpan.com. All rights reserved.
+   This program can be used for any non-commercial purpose freely. */
+
+#include <Servo.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+
+const char* ssid = "MY_WIFI_SSID!!"; // should be changed
+const char* pass = "MY_WIFI_PASS!!"; // should be changed
+
+ESP8266WebServer server(80);
+Servo myservo;
+
+void setup(void){
+  Serial.begin(115200);
+  Serial.println(".");
+  
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  
+  server.on("/", [](){
+    server.send(200, "text/html", "<h1>ON or OFF : <a href=\"/on\">ON</a> &nbsp;&nbsp;or <a href=\"/on\">OFF</a> or URL : /s?degree=90</h1>");
+  });
+
+  server.on("/s", [](){
+    String sdegree = server.arg("degree");
+    int degree = sdegree.toInt();
+    Serial.print("degree: ");
+    Serial.println(degree);
+    myservo.write(degree);
+    server.send(200, "text/plain", String(degree, DEC));
+  });
+
+  server.on("/on", [](){
+    Serial.println("ON");
+    myservo.write(180);
+    server.send(200, "text/html", "<h1>ON! : <a href=\"/off\">OFF</a> or URL : /s?degree=90 </h1>");
+  });
+
+  server.on("/off", [](){
+    Serial.println("OFF");
+    myservo.write(0);
+    server.send(200, "text/html", "<h1>OFF! : <a href=\"/on\">ON</a> or URL : /s?degree=90 </h1>");
+  });
+  
+  server.begin();
+  Serial.println("HTTP server started");
+  
+  myservo.attach(D5);   // Servo attached to D5 pin
+  myservo.write(0);
+}
+ 
+void loop(void){
+  server.handleClient();
+} 
---
+++ neibc_tm1637disp_sonic.ino
@@ -0,0 +1,39 @@
+#include <TM1637Display.h>
+
+// Module connection pins (Digital Pins)
+#define CLK 3
+#define DIO 4
+
+TM1637Display display(CLK, DIO);
+const int trigPin = 8;    //Trig 핀 할당
+const int echoPin = 7;    //Echo 핀 할당
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop()
+{
+  long duration, cm=9999;
+
+  display.setBrightness(0x0f);
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(5);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+
+  cm = duration / 29 / 2;
+  Serial.print(cm);
+  Serial.print("cm");
+  Serial.println();
+
+  display.showNumberDec(cm, true); 
+  delay(100);
+}
+
