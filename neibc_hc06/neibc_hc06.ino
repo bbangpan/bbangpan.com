4f3e538d02038ffa6b6054dea4667065558f901a
bbangpan <tpfs357@gmail.com>
2019-10-23 22:24:49 +0900

    Update neibc_hc06.ino

--- neibc_hc06/neibc_hc06.ino
+++ neibc_hc06/neibc_hc06.ino
@@ -1,34 +1,28 @@
 /* Originally posted on www.bbangpan.com
-   Program Description: HC-06 bluetooth 2.0 / HM-10 BLE test code
-   DataPin : 2,3 to TX, RX of HC-06(cross connection)
-   Tested on : Arduino 1.0.6, Arduino UNO R3, HC-06 or HM-10 BLE
-   
-   Copyright (c) 2015 www.bbangpan.com. All rights reserved.
-   This program can be used for any non-commercial purpose freely.   */
-   
-#include <SoftwareSerial.h>
-  
-SoftwareSerial BTSerial(2, 3); // SoftwareSerial(RX, TX)
-  
-void setup()  
-{
-  Serial.begin(9600);
-  Serial.println("Hello!");
 
-  // set the data rate for the BT port
+Program Description: HC-06 bluetooth 2.0 test code
+DataPin : 2,3 to TX, RX of HC-06(cross connection)
+Tested on : Arduino 1.0.6, Arduino UNO R3, HC-06
+
+Copyright (c) 2015 www.bbangpan.com. All rights reserved.
+This program can be used for any non-commercial purpose freely. */
+
+#include <SoftwareSerial.h>
+
+#define RXPIN 2 //to TX of HC-06
+#define TXPIN 3 //to RX of HC-06
+SoftwareSerial BTSerial(RXPIN, TXPIN);
+ 
+void setup(){
+  Serial.begin(9600);
   BTSerial.begin(9600);
 }
+ 
 void loop(){
-  while (BTSerial.available()){ // if BT sends something
-    byte data = BTSerial.read();
-    Serial.write(data); // write it to serial(serial monitor)
+  if (BTSerial.available()) {
+    Serial.write(BTSerial.read());
   }
-  
-  while (Serial.available()){ // if Serial has input(from serial monitor)
-    byte data = Serial.read();
-
-    BTSerial.write(data); // write it to BT
+  if (Serial.available()) {
+    BTSerial.write(Serial.read());
   }
-  
 }
-
